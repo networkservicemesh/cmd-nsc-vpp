ddc1a00e60ed25380ad03dd282ea338cf576babf
Artem Glazychev <artem.glazychev@xored.com>
2021-02-09 21:51:31 +0700

    Update to latest sdk version
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- main.go
+++ main.go
@@ -47,8 +47,8 @@ import (
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/mechanisms/sendfd"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/metadata"
 	"github.com/networkservicemesh/sdk/pkg/tools/grpcutils"
-	"github.com/networkservicemesh/sdk/pkg/tools/logger"
-	"github.com/networkservicemesh/sdk/pkg/tools/logger/logruslogger"
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
+	"github.com/networkservicemesh/sdk/pkg/tools/log/logruslogger"
 	"github.com/networkservicemesh/sdk/pkg/tools/nsurl"
 	"github.com/networkservicemesh/sdk/pkg/tools/opentracing"
 	"github.com/networkservicemesh/sdk/pkg/tools/spiffejwt"
@@ -74,29 +74,29 @@ func main() {
 	// setup logging
 	// ********************************************************************************
 	logrus.SetFormatter(&nested.Formatter{})
-	logger.EnableTracing(true)
-	ctx, _ = logruslogger.New(
-		logger.WithFields(ctx, map[string]interface{}{"cmd": os.Args[0]}),
-	)
+	log.EnableTracing(true)
+	ctx = log.WithFields(ctx, map[string]interface{}{"cmd": os.Args[0]})
+	ctx = log.WithLog(ctx, logruslogger.New(ctx))
+
 	// ********************************************************************************
 	// Debug self if necessary
 	// ********************************************************************************
 	if err := debug.Self(); err != nil {
-		logger.Log(ctx).Infof("%s", err)
+		log.FromContext(ctx).Infof("%s", err)
 	}
 	starttime := time.Now()
 	// enumerating phases
-	logger.Log(ctx).Infof("there are 6 phases which will be executed followed by a success message:")
-	logger.Log(ctx).Infof("the phases include:")
-	logger.Log(ctx).Infof("1: get config from environment")
-	logger.Log(ctx).Infof("2: run vpp and get a connection to it")
-	logger.Log(ctx).Infof("3: retrieve spiffe svid")
-	logger.Log(ctx).Infof("4: create grpc connection")
-	logger.Log(ctx).Infof("5: create network service client")
-	logger.Log(ctx).Infof("6: connect to all passed services")
-	logger.Log(ctx).Infof("a final success message with start time duration")
+	log.FromContext(ctx).Infof("there are 6 phases which will be executed followed by a success message:")
+	log.FromContext(ctx).Infof("the phases include:")
+	log.FromContext(ctx).Infof("1: get config from environment")
+	log.FromContext(ctx).Infof("2: run vpp and get a connection to it")
+	log.FromContext(ctx).Infof("3: retrieve spiffe svid")
+	log.FromContext(ctx).Infof("4: create grpc connection")
+	log.FromContext(ctx).Infof("5: create network service client")
+	log.FromContext(ctx).Infof("6: connect to all passed services")
+	log.FromContext(ctx).Infof("a final success message with start time duration")
 	// ********************************************************************************
-	logger.Log(ctx).Infof("executing phase 1: get config from environment (time since start: %s)", time.Since(starttime))
+	log.FromContext(ctx).Infof("executing phase 1: get config from environment (time since start: %s)", time.Since(starttime))
 	// ********************************************************************************
 	now := time.Now()
 	config := &Config{}
@@ -106,17 +106,17 @@ func main() {
 	if err := envconfig.Process("nsm", config); err != nil {
 		logrus.Fatalf("error processing config from env: %+v", err)
 	}
-	logger.Log(ctx).Infof("Config: %#v", config)
-	logger.Log(ctx).WithField("duration", time.Since(now)).Infof("completed phase 1: get config from environment")
+	log.FromContext(ctx).Infof("Config: %#v", config)
+	log.FromContext(ctx).WithField("duration", time.Since(now)).Infof("completed phase 1: get config from environment")
 	// ********************************************************************************
-	logger.Log(ctx).Infof("executing phase 2: run vpp and get a connection to it (time since start: %s)", time.Since(starttime))
+	log.FromContext(ctx).Infof("executing phase 2: run vpp and get a connection to it (time since start: %s)", time.Since(starttime))
 	// ********************************************************************************
 	now = time.Now()
 	vppConn, vppErrCh := vpphelper.StartAndDialContext(ctx)
 	exitOnErrCh(ctx, cancel, vppErrCh)
-	logger.Log(ctx).WithField("duration", time.Since(now)).Info("completed phase 2: run vpp and get a connection to it")
+	log.FromContext(ctx).WithField("duration", time.Since(now)).Info("completed phase 2: run vpp and get a connection to it")
 	// ********************************************************************************
-	logger.Log(ctx).Infof("executing phase 3: retrieving svid, check spire agent logs if this is the last line you see (time since start: %s)", time.Since(starttime))
+	log.FromContext(ctx).Infof("executing phase 3: retrieving svid, check spire agent logs if this is the last line you see (time since start: %s)", time.Since(starttime))
 	// ********************************************************************************
 	now = time.Now()
 	source, err := workloadapi.NewX509Source(ctx)
@@ -128,10 +128,10 @@ func main() {
 		logrus.Fatalf("error getting x509 svid: %+v", err)
 	}
 	logrus.Infof("SVID: %q", svid.ID)
-	logger.Log(ctx).WithField("duration", time.Since(now)).Info("completed phase 3: retrieving svid")
+	log.FromContext(ctx).WithField("duration", time.Since(now)).Info("completed phase 3: retrieving svid")
 
 	// ********************************************************************************
-	logger.Log(ctx).Infof("executing phase 4: create grpc connection (time since start: %s)", time.Since(starttime))
+	log.FromContext(ctx).Infof("executing phase 4: create grpc connection (time since start: %s)", time.Since(starttime))
 	// ********************************************************************************
 
 	dialCtx, cancel := context.WithTimeout(ctx, config.DialTimeout)
@@ -155,7 +155,7 @@ func main() {
 	}
 
 	// ********************************************************************************
-	logger.Log(ctx).Infof("executing phase 5: create network service client (time since start: %s)", time.Since(starttime))
+	log.FromContext(ctx).Infof("executing phase 5: create network service client (time since start: %s)", time.Since(starttime))
 	// ********************************************************************************
 
 	c := client.NewClient(
@@ -183,14 +183,14 @@ func main() {
 	}()
 
 	// ********************************************************************************
-	logger.Log(ctx).Infof("executing phase 6: connect to all passed services (time since start: %s)", time.Since(starttime))
+	log.FromContext(ctx).Infof("executing phase 6: connect to all passed services (time since start: %s)", time.Since(starttime))
 	// ********************************************************************************
 
 	for i := 0; i < len(config.NetworkServices); i++ {
 		u := nsurl.NSURL(config.NetworkServices[i])
 		mech := u.Mechanism()
 		if mech.Type != memif.MECHANISM {
-			logger.Log(ctx).Fatalf("mechanism type: %v is not supproted", mech.Type)
+			log.FromContext(ctx).Fatalf("mechanism type: %v is not supproted", mech.Type)
 		}
 		request := &networkservice.NetworkServiceRequest{
 			Connection: &networkservice.Connection{
@@ -203,7 +203,7 @@ func main() {
 		defer cancel()
 		_, err = c.Request(requestCtx, request)
 		if err != nil {
-			logger.Log(ctx).Fatalf("requset has failed: %v", err.Error())
+			log.FromContext(ctx).Fatalf("requset has failed: %v", err.Error())
 		}
 	}
 
@@ -215,13 +215,13 @@ func exitOnErrCh(ctx context.Context, cancel context.CancelFunc, errCh <-chan er
 	// If we already have an error, log it and exit
 	select {
 	case err := <-errCh:
-		logger.Log(ctx).Fatal(err)
+		log.FromContext(ctx).Fatal(err)
 	default:
 	}
 	// Otherwise wait for an error in the background to log and cancel
 	go func(ctx context.Context, errCh <-chan error) {
 		err := <-errCh
-		logger.Log(ctx).Error(err)
+		log.FromContext(ctx).Error(err)
 		cancel()
 	}(ctx, errCh)
 }
