07fe3abc36878d410e3a7adca512ef7f696d23f9
Artem Glazychev <artem.glazychev@xored.com>
2021-12-10 16:05:15 +0700

    Manual sdk update (#331)
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- main.go
+++ main.go
@@ -75,8 +75,7 @@ func main() {
 	// ********************************************************************************
 	logrus.SetFormatter(&nested.Formatter{})
 	log.EnableTracing(true)
-	ctx = log.WithFields(ctx, map[string]interface{}{"cmd": os.Args[0]})
-	ctx = log.WithLog(ctx, logruslogger.New(ctx))
+	ctx = log.WithLog(ctx, logruslogger.New(ctx, map[string]interface{}{"cmd": os.Args[0]}))
 
 	// ********************************************************************************
 	// Debug self if necessary
